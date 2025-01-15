bb2cd76377d1ceaa86466ef0a3329f54333e2228
ARNOLD87A <164343562+ARNOLD87A@users.noreply.github.com>
2025-01-15 10:11:04 +0000

    add comments.js

---
+++ comments.js
@@ -0,0 +1,20 @@
+// create web server with express
+const express = require('express');
+const app = express();
+const port = 3000;
+
+// import comments array
+const comments = require('./comments.js');
+
+// set static folder
+app.use(express.static('public'));
+
+// create get route
+app.get('/comments', (req, res) => {
+  res.json(comments);
+});
+
+// start server
+app.listen(port, () => {
+  console.log(`Server started on port ${port}`);
+});
